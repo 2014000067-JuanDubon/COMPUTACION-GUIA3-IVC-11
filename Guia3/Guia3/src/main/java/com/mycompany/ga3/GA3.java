0d3125354aeb2d578b6e5dbb6b54c4d5ca0b3547
2014000067-JuanDubon <j.dubon.5adb@gmail.com>
2026-04-24 22:55:23 -0600

    Add files via upload

---
+++ Guia3/Guia3/src/main/java/com/mycompany/ga3/GA3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ga3;
+
+/**
+ *
+ * @author H4H7X
+ */
+public class GA3 {
+
+    public static void main(String[] args) {
+        new vent().setVisible(true);
+    }
+}
---
+++ Guia3/README.md
@@ -0,0 +1,1 @@
+# 22_JoseMontenegro_GA3_Computacion-IIU
